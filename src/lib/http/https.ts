4b43c17d44cbb4d4608019482695553e037a3a50
cl3ment_ch <chienher@hotmail.com>
2024-07-21 22:34:14 +0800

    feat: :sparkles: api improvement

--- src/lib/http/https.ts
+++ src/lib/http/https.ts
@@ -14,43 +14,74 @@ interface APIOptions {
   useToken?: boolean;
 }
 
-export const api = async <T = any>(
-  method: APIMethod,
-  resource: string,
-  { data, useToken = false }: APIOptions = {}
-): Promise<APIResponse<T>> => {
-  try {
-    // Construct query string for GET requests
-    const queryString =
-      method === "GET" && data
-        ? "?" + new URLSearchParams(data).toString()
-        : "";
+class API {
+  private async request<T = any>(
+    method: APIMethod,
+    resource: string, //url endpoint
+    { data, useToken = false }: APIOptions = {}
+  ): Promise<APIResponse<T>> {
+    try {
+      const queryString =
+        method === "GET" && data
+          ? "?" + new URLSearchParams(data).toString()
+          : "";
 
-    const headers: HeadersInit = {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    };
+      const headers: HeadersInit = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      };
 
-    if (useToken) {
-      const tokenApi = Cookies.get("accessToken");
-      if (tokenApi) {
-        headers.Authorization = `Bearer ${tokenApi}`;
-      } else {
-        throw new Error("No access token available");
+      if (useToken) {
+        const tokenApi = Cookies.get("accessToken");
+        if (tokenApi) {
+          headers.Authorization = `Bearer ${tokenApi}`;
+        } else {
+          throw new Error("No access token available");
+        }
       }
+
+      const response = await fetch(`${urls.apiBase}${resource}${queryString}`, {
+        method,
+        mode: "cors",
+        headers,
+        body: method !== "GET" ? JSON.stringify(data) : null,
+      });
+
+      const resp: APIResponse<T> = await response.json();
+      return resp;
+    } catch (err) {
+      console.error("API call error:", err);
+      throw err;
     }
-
-    const response = await fetch(`${urls.apiBase}${resource}${queryString}`, {
-      method,
-      mode: "cors",
-      headers,
-      body: method !== "GET" ? JSON.stringify(data) : null,
-    });
-
-    const resp: APIResponse<T> = await response.json();
-    return resp;
-  } catch (err) {
-    console.error("API call error:", err);
-    throw err;
   }
-};
+
+  public get<T = any>(
+    resource: string,
+    options?: APIOptions
+  ): Promise<APIResponse<T>> {
+    return this.request("GET", resource, options);
+  }
+
+  public post<T = any>(
+    resource: string,
+    options?: APIOptions
+  ): Promise<APIResponse<T>> {
+    return this.request("POST", resource, options);
+  }
+
+  public put<T = any>(
+    resource: string,
+    options?: APIOptions
+  ): Promise<APIResponse<T>> {
+    return this.request("PUT", resource, options);
+  }
+
+  public delete<T = any>(
+    resource: string,
+    options?: APIOptions
+  ): Promise<APIResponse<T>> {
+    return this.request("DELETE", resource, options);
+  }
+}
+
+export const api = new API();
